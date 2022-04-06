4e7a0f908f0b6044a415f9276dbf3c2ad0221aca
Carlos Sanabria <Sanabria26mc@gmail.com>
2022-04-05 19:19:54 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record){
+    const finalResult =
+    record.find(element => element.result === 'W')
+    return finalResult ?
+    finalResult.year : undefined
+}
